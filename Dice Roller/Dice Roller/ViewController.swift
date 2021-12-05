5a6de8d2ec0da71d042cafe4a365fee7d5bfdb29
TonyT415 <tonyt41505@gmail.com>
2021-12-05 13:25:07 -0800

    Started Main menu

---
+++ Dice Roller/Dice Roller/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Dice Roller
+//
+//  Created by Tony Tran on 12/5/21.
+//
+
+import UIKit
+
+class ViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
