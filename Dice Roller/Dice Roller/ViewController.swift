64e0a54780d667129206dddd5f25c71303c1e68f
TonyT415 <tonyt41505@gmail.com>
2021-12-08 16:33:15 -0800

    started quick dice

--- Dice Roller/Dice Roller/ViewController.swift
+++ Dice Roller/Dice Roller/ViewController.swift
@@ -14,7 +14,15 @@ class StartViewController: UIViewController {
         // Do any additional setup after loading the view.
         title = "Main"
     }
+    
+    func rand(max:Int) -> Int{
+        return Int.random(in: 1...max)
+    }
 
-
+    @IBAction func d4(_ sender: Any) {
+        d4value.isHidden = false
+        d4value.text = "\(rand(max: 4))"
+    }
+    @IBOutlet weak var d4value: UILabel!
 }
 
